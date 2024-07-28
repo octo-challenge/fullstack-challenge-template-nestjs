38cd63c50b75f33a3bfb07ea13797b57c91d6000
Hansanghyeon <999@hyeon.pro>
2024-07-28 08:06:06 +0000

    feat: api 문서 추가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { apiReference } from '@scalar/nestjs-api-reference'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -9,7 +11,35 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
     credentials: true,
-  });
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('fullstack-challenge-template-nestjs')
+    .setDescription('The account API description')
+    .setVersion('1.0')
+    .addTag('template')
+    //JWT 토큰 설정
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  app.use(
+    '/api',
+    apiReference({
+      spec: {
+        content: document,
+      },
+      hideModels: true,
+    }),
+  )
+
   await app.listen(3000)
 }
 bootstrap()
