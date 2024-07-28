600fd76d5e3a6bbf59f78f5da81fc1e27d253d9c
Hansanghyeon <999@hyeon.pro>
2024-07-28 08:01:11 +0000

    feat: db 설정값 env로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,16 +10,22 @@ import { CatsService } from './cats/cats.service'
 import { CatsModule } from './cats/cats.module'
 import { Cat } from './cats/entity/cats.entity'
 import { CookiesModule } from './cookies/cookies.module';
+import { ConfigModule } from '@nestjs/config'
+import { pipe, flow } from 'fp-ts/function'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.prod']
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'password',
-      database: 'account',
+      host: process.env.DB_HOST,
+      port: pipe(process.env.DB_PORT, Number),
+      username: pipe(process.env.DB_USER),
+      password: pipe(process.env.DB_PASSWORD),
+      database: pipe(process.env.DB_NAME),
       entities: [User, UserAuthority, Cat],
       synchronize: true,
     }),
