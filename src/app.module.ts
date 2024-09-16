d51917b4e155996eef3dcdc4b173f76582cfa0d0
Hansanghyeon <999@hyeon.pro>
2024-09-16 04:17:02 +0000

    feat: remove cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from './auth/auth.module'
 import { User } from './auth/entity/user.entity'
 import { UserAuthority } from './auth/entity/user-authority.entity'
-import { CatsController } from './cats/cats.controller'
-import { CatsService } from './cats/cats.service'
-import { CatsModule } from './cats/cats.module'
-import { Cat } from './cats/entity/cats.entity'
 import { CookiesModule } from './cookies/cookies.module'
 import { ConfigModule } from '@nestjs/config'
 import { pipe, flow } from 'fp-ts/function'
@@ -26,14 +22,13 @@ import { pipe, flow } from 'fp-ts/function'
       username: pipe(process.env.DB_USER),
       password: pipe(process.env.DB_PASSWORD),
       database: pipe(process.env.DB_NAME),
-      entities: [User, UserAuthority, Cat],
+      entities: [User, UserAuthority],
       synchronize: true,
     }),
     AuthModule,
-    CatsModule,
     CookiesModule,
   ],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
